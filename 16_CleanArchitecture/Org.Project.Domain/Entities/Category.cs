738b9a8b1c5057feda93fb7573267b4024d74aad
nihkilrstg18 <nikhilrstg18@gmail.com>
2021-08-02 20:25:29 +0530

    +gitignore = renamed sln to Org.Project +Org.Project.Domain proj +Org.Project.Domain.Entities dir

---
+++ 16_CleanArchitecture/Org.Project.Domain/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Org.Project.Domain.Entities
+{
+    public class Category
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Event> Events { get; set; }
+    }
+}
